9a09afd895fd1a6b9704a042666be4372bb210d6
AlvaroIsrael <israel.nunes.leite@gmail.com>
2022-04-17 20:18:34 -0300

    feat(👑): Enables ssr in styled components.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,35 @@
 // Rendered only once the initial HTML document is loaded
 // noinspection HtmlRequiredTitleElement
 
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document';
 import { ReactElement } from 'react';
+import { ServerStyleSheet } from 'styled-components';
+
+type DocumentProps = DocumentInitialProps & {
+  styles: ReactElement[];
+};
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [...(initialProps.styles as ReactElement[]), ...sheet.getStyleElement()],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
   render(): ReactElement {
     return (
       <Html lang='pt'>
