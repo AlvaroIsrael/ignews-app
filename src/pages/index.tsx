e9d3561debebd1f180cd265bcd6bd3c8e87f808d
AlvaroIsrael <israel.nunes.leite@gmail.com>
2022-04-17 19:00:17 -0300

    feat(👑): Adds subscribe now button to home page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import { Container, Hero } from './styles';
+import { SubscribeButton } from './components/SubscribeButton';
 
 export default function Home(): ReactElement {
   return (
@@ -19,6 +20,7 @@ export default function Home(): ReactElement {
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </Hero>
         <Image src='/images/avatar.svg' alt='avatar' width={336} height={521} />
       </Container>
