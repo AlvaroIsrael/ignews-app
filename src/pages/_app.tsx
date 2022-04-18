66c5a183e245fda0fd9632f747cd0591f429fa08
AlvaroIsrael <israel.nunes.leite@gmail.com>
2022-04-17 21:21:40 -0300

    fix(🪲): Fixes components path in the app.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 // Rendered everytime the app is loaded
 import { AppProps } from 'next/app';
 import GlobalStyle from '../styles/global';
-import { Header } from '../_components/Header';
+import { Header } from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
