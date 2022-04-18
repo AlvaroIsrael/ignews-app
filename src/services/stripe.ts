562d109212c55f876a1ee6fe6a91c2d7f821ec6c
AlvaroIsrael <israel.nunes.leite@gmail.com>
2022-04-18 16:43:14 -0300

    fix(🪲): Fixes build warn.

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,5 +1,5 @@
 import Stripe from 'stripe';
-import * as packageInfo from '../../package.json';
+import packageInfo from '../../package.json';
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY || '', {
   apiVersion: '2020-08-27',
