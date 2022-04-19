ea7c455dc386f2e0c9fb09eb985640fbafa4f177
AlvaroIsrael <israel.nunes.leite@gmail.com>
2022-04-19 12:20:52 -0300

    feat(👑): Adds GitHub integration to signIn button.

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,19 @@
 import { ReactElement } from 'react';
 import { FaGithub } from 'react-icons/fa';
+import { signIn, signOut, useSession } from 'next-auth/client';
 import { Button, CloseIcon } from './styles';
 
 export function SignInButton(): ReactElement {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
-    <Button type='button'>
-      <FaGithub color='#eba417' />
+  const [session] = useSession();
+
+  return session ? (
+    <Button type='button' onClick={() => signOut()}>
+      <FaGithub color='#04d361' />
       Alvaro Israel
       <CloseIcon />
     </Button>
   ) : (
-    <Button type='button'>
+    <Button type='button' onClick={() => signIn('github')}>
       <FaGithub color='#eba417' />
       Sign in with Github
     </Button>
