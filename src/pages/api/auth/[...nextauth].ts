5df5638a000fefaea07531634d932f52b49f7efe
AlvaroIsrael <israel.nunes.leite@gmail.com>
2022-04-27 13:32:39 -0300

    feat(👑): Saving newly logged user to the database.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
-import NextAuth from 'next-auth';
+import { query as q } from 'faunadb';
+import NextAuth, { User } from 'next-auth';
 import Providers from 'next-auth/providers';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +11,30 @@ export default NextAuth({
       scope: 'read:user',
     }),
   ],
+  jwt: {
+    secret: process.env.JWT_SECRET || '',
+  },
+  callbacks: {
+    async signIn(user: User): Promise<boolean> {
+      const { email } = user;
+      const userMail = (email || '').toLowerCase();
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(userMail)))),
+            q.Create(q.Collection('users'), {
+              data: {
+                email: userMail,
+              },
+            }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(userMail))),
+          ),
+        );
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+  },
 });
